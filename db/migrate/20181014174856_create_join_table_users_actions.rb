b2e92faf4af1bd8a5df828c73c079fba0b431d54
Adam Walter <Waltxr@users.noreply.github.com>
2018-10-14 14:39:07 -0400

    migrations

--- db/migrate/20181014174856_create_join_table_users_actions.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableUsersActions < ActiveRecord::Migration[5.1]
-  def change
-    create_join_table :users, :actions do |t|
-      # t.index [:user_id, :action_id]
-      # t.index [:action_id, :user_id]
-    end
-  end
-end
