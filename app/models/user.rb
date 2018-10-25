8b62e26d7f73440d458893f5e961e8fcbe63033d
Adam Walter <Waltxr@users.noreply.github.com>
2018-10-24 23:33:45 -0400

    spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :reported_actions, :class_name => 'Action', :foreign_key => 'reporter_id'
   has_many :received_actions, :class_name => 'Action', :foreign_key =>  'receiver_id'
 
-  #TODO: need to rethink names and behaviors of these relationshps. So far it works for grievances only, and even that model could have several receivers. don't box yourself in via architecture. 
+  #TODO: need to rethink names and behaviors of these relationshps. So far it works for grievances only, and even that model could have several receivers. don't box yourself in via architecture.
 
   has_many :reported_grievances, through: :reported_actions, source: :actionable, source_type: 'Grievance'
   has_many :received_grievances, through: :received_actions, source: :actionable, source_type: 'Grievance'
@@ -16,7 +16,7 @@ class User < ApplicationRecord
   has_many :reported_quotes, through: :reported_actions, source: :actionable, source_type: 'Quote'
   has_many :received_quotes, through: :received_actions, source: :actionable, source_type: 'Quote'
 
-  #needs better names
+  #needs better names...
   has_many :reported_sources, through: :reported_actions, source: :actionable, source_type: 'Source'
   has_many :received_sources, through: :received_actions, source: :actionable, source_type: 'Source'
 
