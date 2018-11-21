4a2a7569eca5e93e07ce3883f9e77b7d0e88d3b4
Adam Walter <Waltxr@users.noreply.github.com>
2018-11-21 18:16:26 -0500

    migrated to rails api only

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,22 +2,21 @@ class User < ApplicationRecord
   has_many :reported_actions, :class_name => 'Action', :foreign_key => 'reporter_id'
   has_many :received_actions, :class_name => 'Action', :foreign_key =>  'receiver_id'
 
-  has_many :reported_grievances, through: :reported_actions, source: :actionable, source_type: 'Grievance'
+  has_many :filed_grievances, through: :reported_actions, source: :actionable, source_type: 'Grievance'
   has_many :received_grievances, through: :received_actions, source: :actionable, source_type: 'Grievance'
 
-  has_many :reported_lols, through: :reported_actions, source: :actionable, source_type: 'Lol'
+  has_many :filed_lols, through: :reported_actions, source: :actionable, source_type: 'Lol'
   has_many :received_lols, through: :received_actions, source: :actionable, source_type: 'Lol'
 
-  has_many :reported_proclamations, through: :reported_actions, source: :actionable, source_type: 'Proclamation'
+  has_many :filed_proclamations, through: :reported_actions, source: :actionable, source_type: 'Proclamation'
   has_many :received_proclamations, through: :received_actions, source: :actionable, source_type: 'Proclamation'
 
-  has_many :reported_quotes, through: :reported_actions, source: :actionable, source_type: 'Quote'
+  has_many :filed_quotes, through: :reported_actions, source: :actionable, source_type: 'Quote'
   has_many :received_quotes, through: :received_actions, source: :actionable, source_type: 'Quote'
 
-  #needs better names...
-  has_many :reported_sources, through: :reported_actions, source: :actionable, source_type: 'Source'
+  has_many :sited_sources, through: :reported_actions, source: :actionable, source_type: 'Source'
   has_many :received_sources, through: :received_actions, source: :actionable, source_type: 'Source'
 
-  has_many :reported_valid_points, through: :reported_actions, source: :actionable, source_type: 'Valid_Points'
-  has_many :received_valid_points, through: :received_actions, source: :actionable, source_type: 'Valid_Points'
+  has_many :rewarded_valid_points, through: :reported_actions, source: :actionable, source_type: 'Valid_Points'
+  has_many :valid_points_awarded, through: :received_actions, source: :actionable, source_type: 'Valid_Points'
 end
