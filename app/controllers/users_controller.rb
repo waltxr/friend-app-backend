e1c8940d7f307ba0e97266a41ded2ad0ed5aacd4
Adam Walter <Waltxr@users.noreply.github.com>
2018-11-22 08:16:43 -0500

    auth working, tested with httpie.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  skip_before_action :authorize_request, only: :create
   # POST /signup
   # return authenticated token upon signup
   def create
@@ -17,5 +18,5 @@ class UsersController < ApplicationController
       :password,
       :password_confirmation
     )
-  end  
+  end
 end
