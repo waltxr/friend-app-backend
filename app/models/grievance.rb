31e46d2a5aeba3d5d1cc652d7ce85962baccf86c
Adam Walter <Waltxr@users.noreply.github.com>
2018-12-10 19:39:14 -0500

    Grievance has multiple receivers.

--- app/models/grievance.rb
+++ app/models/grievance.rb
@@ -1,6 +1,6 @@
 class Grievance < ApplicationRecord
-  # include ReporterReceiver
   include Commentable
+  
   belongs_to :reporter, class_name: 'User'
   has_many :grievance_recipients
   has_many :receivers, through: :grievance_recipients, source: :user
