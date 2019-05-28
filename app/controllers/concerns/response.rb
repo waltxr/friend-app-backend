10cac0b8fdd59d0b0b15d142334968e2b6fcd2e8
Adam Walter <Waltxr@users.noreply.github.com>
2019-05-27 23:34:36 -0400

    attempting to render comments recursively.

--- app/controllers/concerns/response.rb
+++ app/controllers/concerns/response.rb
@@ -12,21 +12,32 @@ module Response
       render json: object, :include => [:reporter, :receivers, {:comments => {:include => :user}}], status: status
   end
 
+  # def json_response_user(object, status = :ok)
+  #     render json: object, :include => [
+  #       {:filed_grievances => {:include => [:receivers, :reporter, {:comments => {:include => :user}}]}},
+  #       {:received_grievances => {:include => [:receivers,:reporter, {:comments => {:include => :user}}]}},
+  #       {:awarded_valid_points => {:include => :reporter}},
+  #       {:received_valid_points => {:include => :receiver}},
+  #       {:awarded_lols => {:include => :reporter}},
+  #       {:received_lols => {:include => :receiver}},
+  #       {:reported_proclamations => {:include => :reporter}},
+  #       {:received_proclamations => {:include => :receiver}},
+  #       {:reported_quotes => {:include => :reporter}},
+  #       {:received_quotes => {:include => :receiver}},
+  #       {:cited_sources => {:include => :reporter}},
+  #       {:received_sources => {:include => :receivers}}
+  #       ], status: status
+  # end
+
+
+  # def merge_comments
+  #   json = to_json.merge(comments: [])
+  #   comments.each { |comment| json[:comments] << item.custom_json }
+  #   json
+  # end
+
   def json_response_user(object, status = :ok)
-      render json: object, :include => [
-        {:filed_grievances => {:include => [:receivers, :reporter, {:comments => {:include => :user}}]}},
-        {:received_grievances => {:include => [:receivers,:reporter, {:comments => {:include => :user}}]}},
-        {:awarded_valid_points => {:include => :reporter}},
-        {:received_valid_points => {:include => :receiver}},
-        {:awarded_lols => {:include => :reporter}},
-        {:received_lols => {:include => :receiver}},
-        {:reported_proclamations => {:include => :reporter}},
-        {:received_proclamations => {:include => :receiver}},
-        {:reported_quotes => {:include => :reporter}},
-        {:received_quotes => {:include => :receiver}},
-        {:cited_sources => {:include => :reporter}},
-        {:received_sources => {:include => :receivers}}
-        ], status: status
+    render json: object, :include => [{:filed_grievances => {:include => [:receivers, :reporter, {:comments => {:include => :user}}]}}]
   end
 
 end
